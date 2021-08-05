dd91506a6ec99962be8f62bddefad37ee8cab80b
Sonny <sonnynorway@gmail.com>
2021-08-05 00:17:40 -0400

    Hardcoded RSA on barebone webapp

---
+++ test.py
@@ -0,0 +1,9 @@
+def hel():
+    return("Hello it Is me")
+
+# def rsa_encrypt(text, p, q)
+    # returns list of encrypted values
+    # [14, 6, 4, 21, 5]
+
+def bye(num, num2):
+    return(str([14, 6, 4, 21, 5]))
