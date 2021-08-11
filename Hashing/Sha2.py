fc5d7934570a22339afc46d7ae23cd38475dfc7a
Raymond Phan <raymondphan00@gmail.com>
2021-08-11 15:50:42 -0400

    Wrote the padding algorithm

--- Hashing/Sha2.py
+++ Hashing/Sha2.py
@@ -1,4 +1,4 @@
-def encrypt(input, privKey, pubKey):
+def encrypt(input, privkey=None, pubKey=None):
     """SHA2 Hashing algorithim
 
     Args:
@@ -6,7 +6,19 @@ def encrypt(input, privKey, pubKey):
         privKey (str): no private key. defaulted to None
         pubKey (str): no public key. defaulted to None
 
+
     Returns:
         str: returns the hash value of input using SHA2 hashing algorithim
     """
+
+    bin_string = ''.join(format(ord(i), '08b') for i in input)
+    original_length = len(bin_string)
+
+    bin_string_with_1 = ''.join(format(ord(i), '08b') for i in input) + "10000000"
+    byte_list = [bin_string_with_1[i:i + 8] for i in range(0, len(bin_string_with_1), 8)]
+
+    while len(byte_list) % 512 != 440:
+        byte_list.append('00000000')
+    byte_list.append(bin(original_length)[2:].zfill(8))
+
     pass
