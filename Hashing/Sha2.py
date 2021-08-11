6f894ff5225551cf52dec2bb503812caee3a48af
Raymond Phan <raymondphan00@gmail.com>
2021-08-11 15:53:48 -0400

    Wrote the padding algorithm

--- Hashing/Sha2.py
+++ Hashing/Sha2.py
@@ -17,8 +17,7 @@ def encrypt(input, privkey=None, pubKey=None):
     bin_string_with_1 = ''.join(format(ord(i), '08b') for i in input) + "10000000"
     byte_list = [bin_string_with_1[i:i + 8] for i in range(0, len(bin_string_with_1), 8)]
 
-    while len(byte_list) % 512 != 440:
+    while len(byte_list) * 8 % 512 != 440:
         byte_list.append('00000000')
     byte_list.append(bin(original_length)[2:].zfill(8))
-
     pass
