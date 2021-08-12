8c78858fff188bbe4325b5b640fac2e0fdc0ea43
Anhton Nguyen <anhton154321@gmail.com>
2021-08-12 01:08:42 -0400

    Anhton's final branch!

--- Hashing/Md5.py
+++ Hashing/Md5.py
@@ -1,12 +1,21 @@
-def encrypt(input, privKey, pubKey):
-    """MD5 Hashing algorithim
 
-    Args:
-        input (str): string data that is to be encrypted
-        privKey (str): no private key. defaulted to None
-        pubKey (str): no public key. defaulted to None
 
-    Returns:
-        str: returns the hash value of input using MD5 hashing algorithim
-    """
-    pass
+def padd(msg):
+    msg_length = len(string_to_binary(msg))
+    pad = "1" + ("0"*(448 - msg_length - 1))
+    length2 = bin(len(string_to_binary(msg)))[2:]
+    length1 = "0"*(64 - len(length2))
+    ret = string_to_binary(msg) + pad + length1 + length2
+    print(ret)
+    print(len(ret))
+
+
+
+
+def string_to_binary(msg):
+    return ''.join(bin(ord(c)) for c in msg).replace('b','')
+
+
+padd("abc")
+
+# print(bin(24)[2:])
